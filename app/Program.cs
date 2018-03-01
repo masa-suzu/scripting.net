0c47bdf2dab2cbe4be9faaf386f7cfe14919c878
masasuzu <szk.masato@icloud.com>
2018-03-02 05:16:03 +0900

    Add a script.

---
+++ app/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args == null || args.Length ==0)
+        {
+            args = new string[]{"scripts/TwoPlusOne.csx"};
+        }
+        MainAsync(args[0]).Wait();
+    }
+    private static async Task MainAsync(string scriptFilePath)
+    {
+        var so = ScriptOptions.Default;
+        try
+        {
+            var script = CSharpScript.Create<int>(System.IO.File.ReadAllText(scriptFilePath),so);
+            var state = await script.RunAsync();
+            Console.WriteLine(state.ReturnValue);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
